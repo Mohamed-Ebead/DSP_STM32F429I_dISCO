22566fb3ac97fba033b1605088716705bb7b5ae4
Mohamed Ebead <mohamedebead08@gmail.com>
2022-10-16 10:06:06 +0200

    Plotting Signals on SWV using internal logic analyzer

---
+++ Core/Inc/Signals.h
@@ -0,0 +1,16 @@
+/*
+ * Signals.h
+ *
+ *  Created on: Oct 15, 2022
+ *      Author: Mohamed  Ebead
+ */
+
+#ifndef INC_SIGNALS_H_
+#define INC_SIGNALS_H_
+
+
+#define _5_HZ_SIGNAL_LEN  301
+
+extern float _5hz_signal[_5_HZ_SIGNAL_LEN];
+
+#endif /* INC_SIGNALS_H_ */
---
+++ Debug/Core/Src/Signals.d
@@ -0,0 +1,2 @@
+Core/Src/Signals.o: ../Core/Src/Signals.c ../Core/Inc/Signals.h
+../Core/Inc/Signals.h:
